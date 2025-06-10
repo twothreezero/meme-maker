1e0ee6d272d4d809a57deaa36eba1560e7d210c2
twothreezero <realtwothreezero@gmail.com>
2025-06-11 02:21:27 +0900

    Change: painting lines based on mouse

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 
-const colors = [
-  "#3498db",
-  "#3498db",
-  "#9b59b6",
-  "#f39c12",
-  "#c0392b",
-  "#34495e",
-  "#2ecc71",
-  "#2980b9",
-  "#ecf0f1",
-  "##7f8c8d",
-];
+let isPainting = false;
 
-function onClick(e) {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+function onMove(e) {
+  if (isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    return;
+  }
+  // ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.effsetY);
+}
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting)
