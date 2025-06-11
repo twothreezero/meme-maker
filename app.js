ae3ecd58418a430dec5ac978fc4705ff7e167676
twothreezero <realtwothreezero@gmail.com>
2025-06-11 16:26:06 +0900

    Add: fill canvas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector("canvas");
 const lineWidth = document.getElementById("line-width")
 const color = document.getElementById("color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.getElementById("mode-btn");
 
 const ctx = canvas.getContext("2d");
 
@@ -11,6 +12,7 @@ canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 function onMove(e) {
   if (isPainting) {
@@ -40,12 +42,28 @@ function onColorClick(e) {
   ctx.fillStyle = colorValue;
   color.value = colorValue;
 }
+function onModeClick(e) {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill"
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw"
+  }
+}
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
