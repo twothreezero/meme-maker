f9fe0cc736569d7b921df3c3144e9d8743670c39
twothreezero <realtwothreezero@gmail.com>
2025-06-11 15:36:21 +0900

    Add: change line width

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const canvas = document.querySelector("canvas");
+const lineWidth = document.getElementById("line-width")
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -12,7 +13,6 @@ function onMove(e) {
     ctx.stroke();
     return;
   }
-  // ctx.beginPath();
   ctx.moveTo(e.offsetX, e.effsetY);
 }
 function startPainting() {
@@ -20,9 +20,15 @@ function startPainting() {
 }
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(e) {
+  ctx.lineWidth = e.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting)
+canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
